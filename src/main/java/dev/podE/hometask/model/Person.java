b5eb6981dad6290ef780f3db49b99cb12b468c70
NJBodam <bodambest@yahoo.com>
2022-01-08 04:33:05 +0100

    second commit
    jdbc template implementation and application property setup for postgresql. To be configured on whoever's system that will handle demonstration

--- src/main/java/dev/podE/hometask/model/Person.java
+++ src/main/java/dev/podE/hometask/model/Person.java
@@ -1,4 +1,59 @@
 package dev.podE.hometask.model;
 
+import java.util.Date;
+
+
 public class Person {
+    private int id;
+    private String name;
+    private String nationality;
+    private Date birthDate;
+
+    public Person(int id, String name, String nationality, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.nationality = nationality;
+        this.birthDate = birthDate;
+    }
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
 }
--- src/main/java/dev/podE/hometask/repository/PersonRepository.java
+++ src/main/java/dev/podE/hometask/repository/PersonRepository.java
@@ -1,4 +1,51 @@
 package dev.podE.hometask.repository;
 
+import dev.podE.hometask.model.Person;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Repository
 public class PersonRepository {
+
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person> getAllPersons() {
+        String sql = "SELECT * FROM PERSON";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Person>(Person.class));
+    }
+
+    public Person getPersonById(int id) {
+        String sql = "SELECT * FROM PERSON WHERE ID = ?";
+        return jdbcTemplate.queryForObject(sql,
+                new BeanPropertyRowMapper<Person>(Person.class),
+                new Object[] {id});
+    }
+
+    public int insertPerson(Person person) {
+        String sql = "INSERT INTO PERSON (ID, Name, Nationality,Birth_date) " +
+                "VALUES (?, ?, ?, ?, )";
+        return jdbcTemplate.update( sql, person.getId(), person.getName(), person.getNationality(),
+                new Timestamp(person.getBirthDate().getTime()));
+    }
+
+    public int updatePlayer(Person person){
+        String sql = "UPDATE PLAYER " +
+                "SET Name = ?, Nationality = ?, Birth_date = ?" +
+                "WHERE ID = ?";
+
+        return jdbcTemplate.update( sql,
+                person.getName(),
+                person.getNationality(),
+                new Timestamp(person.getBirthDate().getTime()),
+                person.getId());
+    }
+
+    public int deletePersonById(int id) {
+        String sql="DELETE FROM PERSON WHERE ID = ?";
+        return jdbcTemplate.update(sql, new Object[] {id});
+    }
 }
