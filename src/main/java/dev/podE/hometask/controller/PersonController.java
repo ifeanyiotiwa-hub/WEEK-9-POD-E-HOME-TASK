60da58432b2e78768a17718aec788db5266b5a4a
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-01-08 15:32:34 +0100

    Modified PersonController Class

--- src/main/java/dev/podE/hometask/controller/PersonController.java
+++ src/main/java/dev/podE/hometask/controller/PersonController.java
@@ -1,4 +1,26 @@
 package dev.podE.hometask.controller;
 
+
+import dev.podE.hometask.model.Person;
+import dev.podE.hometask.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PersonController {
+
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+
+    @PostMapping("/person")
+    public String savePerson(@RequestBody Person person) {
+        personService.savePerson(person);
+        return "Person saved";
+    }
 }
