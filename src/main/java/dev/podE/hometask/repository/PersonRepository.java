b68b72326000ca53f1b3f96518be3cba25a01902
NJBodam <bodambest@yahoo.com>
2022-01-08 04:56:35 +0100

    third commit
    the task only required one method implementation so i deleted the others

--- src/main/java/dev/podE/hometask/repository/PersonRepository.java
+++ src/main/java/dev/podE/hometask/repository/PersonRepository.java
@@ -13,39 +13,12 @@ public class PersonRepository {
 
     JdbcTemplate jdbcTemplate;
 
-    public List<Person> getAllPersons() {
-        String sql = "SELECT * FROM PERSON";
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Person>(Person.class));
-    }
 
-    public Person getPersonById(int id) {
-        String sql = "SELECT * FROM PERSON WHERE ID = ?";
-        return jdbcTemplate.queryForObject(sql,
-                new BeanPropertyRowMapper<Person>(Person.class),
-                new Object[] {id});
-    }
-
-    public int insertPerson(Person person) {
+    public int savePerson(Person person) {
         String sql = "INSERT INTO PERSON (ID, Name, Nationality,Birth_date) " +
                 "VALUES (?, ?, ?, ?, )";
         return jdbcTemplate.update( sql, person.getId(), person.getName(), person.getNationality(),
                 new Timestamp(person.getBirthDate().getTime()));
     }
 
-    public int updatePlayer(Person person){
-        String sql = "UPDATE PLAYER " +
-                "SET Name = ?, Nationality = ?, Birth_date = ?" +
-                "WHERE ID = ?";
-
-        return jdbcTemplate.update( sql,
-                person.getName(),
-                person.getNationality(),
-                new Timestamp(person.getBirthDate().getTime()),
-                person.getId());
-    }
-
-    public int deletePersonById(int id) {
-        String sql="DELETE FROM PERSON WHERE ID = ?";
-        return jdbcTemplate.update(sql, new Object[] {id});
-    }
 }
