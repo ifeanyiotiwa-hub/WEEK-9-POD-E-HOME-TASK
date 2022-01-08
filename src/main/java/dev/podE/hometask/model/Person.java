947ef6d45656ef65d97d0dcb3fee3659f2725a27
Odochi-Okara <85240161+Odochi-Okara@users.noreply.github.com>
2022-01-08 11:03:28 +0100

    created user model

--- src/main/java/dev/podE/hometask/model/Person.java
+++ src/main/java/dev/podE/hometask/model/Person.java
@@ -1,59 +1,22 @@
 package dev.podE.hometask.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
-
-
+// Odochi Okara
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
     private int id;
-    private String name;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
     private String nationality;
     private Date birthDate;
 
-    public Person(int id, String name, String nationality, Date birthDate) {
-        this.id = id;
-        this.name = name;
-        this.nationality = nationality;
-        this.birthDate = birthDate;
-    }
 
-    public Person() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", nationality='" + nationality + '\'' +
-                ", birthDate=" + birthDate +
-                '}';
-    }
 }
