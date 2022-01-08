cee64b5b066718f002566e16175180b69230c991
NJBodam <bodambest@yahoo.com>
2022-01-08 11:55:19 +0100

    edited person repository
    added new fields created by odochi

--- src/main/java/dev/podE/hometask/repository/PersonRepository.java
+++ src/main/java/dev/podE/hometask/repository/PersonRepository.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -13,12 +14,16 @@ public class PersonRepository {
 
     JdbcTemplate jdbcTemplate;
 
+    public PersonRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public int savePerson(Person person) {
-        String sql = "INSERT INTO PERSON (ID, Name, Nationality,Birth_date) " +
-                "VALUES (?, ?, ?, ?, )";
-        return jdbcTemplate.update( sql, person.getId(), person.getName(), person.getNationality(),
+        String sql = "INSERT INTO PERSON (ID, First_name, Last_name, Email, Password, Nationality, Birth_date) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?)";
+        return jdbcTemplate.update( sql, person.getId(), person.getFirstName(), person.getLastName(), person.getEmail(), person.getPassword(), person.getNationality(),
                 new Timestamp(person.getBirthDate().getTime()));
     }
 
 }
+
