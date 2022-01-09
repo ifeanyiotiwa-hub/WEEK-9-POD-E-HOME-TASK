288146c81d2d7d4b4e931af983cd470360a87e8c
Emeka <emkychuks062@gmail.com>
2022-01-09 20:41:32 +0100

    Modified PersonRepository Class with the correct DB column names

--- src/main/java/dev/podE/hometask/repository/PersonRepository.java
+++ src/main/java/dev/podE/hometask/repository/PersonRepository.java
@@ -1,28 +1,41 @@
 package dev.podE.hometask.repository;
 
 import dev.podE.hometask.model.Person;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.List;
+import java.sql.ResultSet;
 
 @Repository
 public class PersonRepository {
 
     JdbcTemplate jdbcTemplate;
 
+    @Autowired
     public PersonRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     public int savePerson(Person person) {
-        String sql = "INSERT INTO person (ID, first_name, last_name, email, password, nationality, birth_date) " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?)";
-        return jdbcTemplate.update( sql, person.getId(), person.getFirstName(), person.getLastName(), person.getEmail(), person.getPassword(), person.getNationality(),
-                new Timestamp(person.getBirthDate().getTime()));
+        String sql = "INSERT INTO person (first_name, last_name, email, password, nationality, birth_date) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+
+        return jdbcTemplate.update( sql, person.getFirstName(), person.getLastName(), person.getEmail(), person.getPassword(), person.getNationality(),
+                person.getBirthDate());
+    }
+
+    public void deletePerson(int id){
+        String sql = "DELETE  FROM person WHERE id=?";
+         jdbcTemplate.update( sql,id);
+    }
+
+    public void updatePerson(Person person){
+        String sql = "UPDATE person SET first_name=?, last_name=?, " +
+                "email=?, password = ?, nationality=?, birth_date=? WHERE id=?";
+        Object [] params = {person.getFirstName(), person.getLastName(), person.getEmail(), person.getPassword(), person.getNationality(),
+                person.getBirthDate(),person.getId()};
+        jdbcTemplate.update(sql,params);
     }
 
 }
