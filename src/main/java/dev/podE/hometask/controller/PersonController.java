2abd8ce7d1c62c1b18c6a8fcf4b5495fdd0994b7
Emeka <emkychuks062@gmail.com>
2022-01-09 20:39:44 +0100

    Modified PersonController class to include other request mapping methods

--- src/main/java/dev/podE/hometask/controller/PersonController.java
+++ src/main/java/dev/podE/hometask/controller/PersonController.java
@@ -4,9 +4,11 @@ package dev.podE.hometask.controller;
 import dev.podE.hometask.model.Person;
 import dev.podE.hometask.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PersonController {
@@ -22,5 +24,28 @@ public class PersonController {
     public String savePerson(@RequestBody Person person) {
         personService.savePerson(person);
         return "Person saved";
+    }
+
+    @DeleteMapping(path = "person/{id}")
+    public String deletePerson(@PathVariable("id") int id){
+        personService.deletePerson(id);
+        return "person with id "+id+ " deleted";
+
+    }
+    @PutMapping(path = "person/{id}")
+    public String updatePerson(@PathVariable("id")int id, @RequestBody Person person){
+        personService.updatePerson(person,id);
+        return "person with the id of "+id+" updated";
+    }
+    @GetMapping(path = "person/{id}")
+    public  String getPersonById(@PathVariable("id")int id){
+        Person person = new Person();
+        person.setId(id);
+        return person+" with id "+id+ " gotten from the DB";
+    }
+
+    public List<Person> getAllPersons(){
+        List<Person> personList = new ArrayList<>();
+        return  personList;
     }
 }
