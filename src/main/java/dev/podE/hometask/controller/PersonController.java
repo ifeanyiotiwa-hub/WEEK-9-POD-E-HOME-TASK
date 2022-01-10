ab1122fd1127d7df727b213f891a7b5af3c86f80
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-01-10 09:18:58 +0100

    Revert "Modified PersonController to handle NPE"
    This reverts commit 46a93e2c43cc18eeaf6a23a03cbd6630e2ed6d07.

--- src/main/java/dev/podE/hometask/controller/PersonController.java
+++ src/main/java/dev/podE/hometask/controller/PersonController.java
@@ -61,7 +61,6 @@ public class PersonController {
 
         responseData.setSubject(subject);
         responseData.setPerson(person);
-        Objects.requireNonNull(subject);
         person.setKey(subject.getKey());
         return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
